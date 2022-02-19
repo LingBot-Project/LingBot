e8668621f70397f2cf2cebfeab48497011803d08
guimc233 <65482560+guimc233@users.noreply.github.com>
2022-02-20 00:11:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def acg_img():
     try:
         a = "https://img.xjh.me/random_img.php?return=json"
         a1 = requests.get(url=a).json()
-        return base64.b64encode(requests.get(url=a1["img"]).content).decode()
+        return base64.b64encode(requests.get(url='https:'+a1["img"]).content).decode()
     except Exception as e:
         return text2image("获取图片失败\n"+traceback.format_exc())
     
