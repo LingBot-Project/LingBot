d95541142e013f1fb52c9126295d6bd9e4b5c4d2
guimc233 <admin@guimc.ltd>
2022-03-30 11:03:20 +0800

    aua

--- main.py
+++ main.py
@@ -622,9 +622,9 @@ UP主: {str1["owner"]["name"]} ({str1["owner"]["mid"]})
                 pI["karma"] = "这个玩家并没有登录过Hypixel, 无法查询"
             playerSkin = requests.get("https://crafatar.com/renders/body/" + pI["uuid"])
             pmsg = "---查询结果---\n玩家名称: [{rank}]{name}\n等级: {level}\nKarma(人品值): {karma}\n上次登陆: {last_login}\n首次登陆: {first_login}".format(
-                rank=pI["rank"]["rank"].replace(" ", "").replace("PLUS", "+"),
+                rank=player1.getRank(),
                 name=pI["displayName"],
-                level=pI["networkLevel"],
+                level=player1.getLevel(),
                 karma=pI["karma"],
                 last_login=datetime.datetime.utcfromtimestamp(pI["lastLogin"] / 1000).strftime("%Y-%m-%d %H:%M:%S"),
                 first_login=datetime.datetime.utcfromtimestamp(pI["firstLogin"] / 1000).strftime("%Y-%m-%d %H:%M:%S"))
