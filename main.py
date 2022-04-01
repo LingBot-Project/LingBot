486372bfd7654753ba803341b7b80394fffbd098
siuank <y2915324611@163.com>
2022-04-01 15:30:58 +0800

    Upgrade anti spam -> FIX

--- main.py
+++ main.py
@@ -377,7 +377,7 @@ def on_message2(ws, message):
         if msg.sender.id not in SPAM2_VL:
             SPAM2_VL[msg.sender.id] = 0
         _spam_cre = get_min_distance(str(SPAM2_MSG[msg.sender.id]).lower(), msg.text.lower())
-        if _spam_cre <= 0.15 and len(msg.text) >= 4:
+        if _spam_cre <= 0.15 and len(msg.text) >= 4 and not msg.sender.id == 2854196310:
             SPAM2_VL[msg.sender.id] += 10
             if _spam_cre <= 0.01:
                 SPAM2_VL[msg.sender.id] += 25
