5a09d8923bcafa856dad8f6e0c7b0965c59beca9
unrealizablelingyi <102005228+unrealizablelingyi@users.noreply.github.com>
2022-04-02 15:54:22 +0800

    math

--- main.py
+++ main.py
@@ -572,16 +572,16 @@ UP主: {str1["owner"]["name"]} ({str1["owner"]["mid"]})
             msg.fast_reply(str(int(command_list[1])+int(command_list[2])))
             return
         if command_list [0] == "!subtract":
-            msg.fast_reply(str(int(command_list[1]-int(command_list[2]))))
+            msg.fast_reply(str(int(command_list[1])-int(command_list[2])))
             return
         if command_list [0] == "!multiply":
-            msg.fast_reply(str(int(command_list[1]*int(command_list[2]))))
+            msg.fast_reply(str(int(command_list[1])*int(command_list[2])))
             return
         if command_list [0] == "!divide":
-            msg.fast_reply(str(int(command_list[1]/int(command_list[2]))))
+            msg.fast_reply(str(int(command_list[1])/int(command_list[2])))
             return
         if command_list [0] == "!involution":
-            msg.fast_reply(str(int(command_list[1]^int(command_list[2]))))
+            msg.fast_reply(str(int(command_list[1])^int(command_list[2])))
             return
         
         if command_list[0] == "!tcping":
