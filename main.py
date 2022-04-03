0b1aa3c5e8a4c9806ba6b5d6811d2de291d34921
guimc233 <guimc_awa233@outlook.com>
2022-04-04 00:36:24 +0800

    Updated

--- main.py
+++ main.py
@@ -1023,8 +1023,7 @@ def on_close(ws, a, b):
 
 
 def goodmor(target=None):
-    msg1 = "早上好呀~ [CQ:image,file=base64://{}][CQ:image,file=base64://{}]".format(
-        text2image(requests.get(url="https://www.ipip5.com/today/api.php?type=txt", verify=False).text),
+    msg1 = "早上好呀~ [CQ:image,file=base64://{}]".format(
         requests.post(url="http://localhost:25666/url2base64", data={"url": "https://news.topurl.cn/"}).text.replace("\n", ""))
     s = getGroups()
     if target:
