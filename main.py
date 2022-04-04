928510e5a3a502c74c930b420c56ca29157fb0a1
Ossicles <2775367578@qq.com>
2022-04-04 14:59:08 +0800

    test

--- main.py
+++ main.py
@@ -706,7 +706,7 @@ UP主: {str1["owner"]["name"]} ({str1["owner"]["mid"]})
                     unmutePerson(command_list[1], command_list[2])
                 else:
                     mutePerson(command_list[1], command_list[2], command_list[3] * 60)
-                    msg.fast_reply("已尝试在群 {} 禁言 {} {}分钟 {}".format(command_list[1], command_list[2], command_list[3], command_list))
+                    msg.fast_reply(f"已尝试在群 {command_list[1]} 禁言 {command_list[2]} {command_list[3]}分钟 {command_list}")
             else:
                 msg.fast_reply("你的权限不足!")
 
