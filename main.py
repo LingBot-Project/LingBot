1d06cc024cb640b2f256d18253853893a5998173
Ossicles <q2775367578@gmail.com>
2022-04-05 20:53:57 +0800

    fix bug

--- main.py
+++ main.py
@@ -746,6 +746,10 @@ UP主: {str1["owner"]["name"]} ({str1["owner"]["mid"]})
                 msg.fast_reply("你的权限不足!")
 
         if command_list[0] == "!我要自闭":
+            if len(command_list) == 1:
+                AUTISM.append((msg.group.id, msg.sender.id))
+                msg.fast_reply("您想自闭多久呢？")
+                return
             command_list[1] = int(command_list[1])
             mute_time = command_list[1] * 60
             time_type = 'min'
@@ -764,6 +768,22 @@ UP主: {str1["owner"]["name"]} ({str1["owner"]["mid"]})
                 f"您将要自闭{command_list[1]}{mute_type(time_type)}")
 
         if (msg.group.id, msg.sender.id) in AUTISM:
+            command_list[0] = int(command_list[0])
+            mute_time = command_list[0] * 60
+            time_type = 'min'
+            if len(command_list) > 1:
+                if command_list[1] == 's' or command_list[1] == '秒':
+                    time_type = 's'
+                    mute_time = command_list[1]
+                if command_list[1] == 'h' or command_list[1] == '小时':
+                    time_type = 'h'
+                    mute_time = command_list[1] * 3600
+                if command_list[1] == 'd' or command_list[1] == '天':
+                    time_type = 'd'
+                    mute_time = command_list[1] * 86400
+            mutePerson(msg.group.id, msg.sender.id, mute_time)
+            msg.fast_reply(
+                f"您将要自闭{command_list[0]}{mute_type(time_type)}")
             AUTISM.remove((msg.group.id, msg.sender.id))
 
         if command_list[0] == "!vl":
