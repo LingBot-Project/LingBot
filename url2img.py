b7f2e717fcc519a66f4a56cd06509181c52f02b1
guimc233 <guimc_awa233@outlook.com>
2022-04-05 09:38:14 +0800

    Updated

--- url2img.py
+++ url2img.py
@@ -39,6 +39,7 @@ def screenshot(url: str, img_path: str) -> None:
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument("window-size=1920,1080")
     chrome_options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
     driver1 = webdriver.Chrome(chrome_options=chrome_options, executable_path="/root/chromedriver")
     driver1.get(url)
