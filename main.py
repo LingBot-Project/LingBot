e787fe5476cfd5368b303831f36b076892761d87
Ossicles <q2775367578@gmail.com>
2022-04-06 18:17:24 +0800

    fix bug

--- main.py
+++ main.py
@@ -975,10 +975,10 @@ Coins: {coin_purse}
             msg.fast_reply(pmsg)
 
         if command_list[0] == "!introduce" or command_list[0] == "!介绍":
-            if not os.path.exists("introduce_data.json"):
-                w = open("introduce_data.json", 'w')
-                w.write("""{"qq":{}}""")
-                w.close()
+            #if not os.path.exists("introduce_data.json"):
+            #    w = open("introduce_data.json", 'w')
+            #    w.write("""{"qq":{}}""")
+            #    w.close()
             if command_list[1] == 'help':
                 sendMessage(f"""
 介绍指令 : !introduce/!介绍
@@ -989,18 +989,18 @@ Coins: {coin_purse}
     <群号> : 如果在本群可以填"this"
     <介绍> : 随便填（支持CQ码）
 查看介绍 : 
-    发送“!introduce/!介绍 c <q号>”  
+    发送“!introduce/!介绍 <q号>”  
 """, target_group=msg.group.id)
-            elif command_list[1] == "c":
+            elif command_list[1].isdigit():
                 with open("introduce_data.json", "r+", encoding='utf-8') as introduce_json:
                     data = json.load(introduce_json)
-                    if command_list[2] in data['qq']:
-                        if msg.group.id in data['qq'][command_list[2]]:
-                            sendMessage(f"的简介为 : \n{data['qq'][command_list[2]][msg.group.id]}", command_list[1], msg.group.id)
+                    if command_list[1] in data['qq']:
+                        if msg.group.id in data['qq'][command_list[1]]:
+                            sendMessage(f"的简介为 : \n{data['qq'][command_list[1]][msg.group.id]}", command_list[1], msg.group.id)
                         else:
-                            sendMessage(f"{data['qq'][command_list[2]][msg.group.id]}未在此群发布介绍", command_list[1], msg.group.id)
+                            sendMessage(f"{data['qq'][command_list[1]][msg.group.id]}未在此群发布介绍", command_list[1], msg.group.id)
                     else:
-                        sendMessage(f"{data['qq'][command_list[2]][msg.group.id]}未在任何群发布介绍", command_list[1], msg.group.id)
+                        sendMessage(f"{data['qq'][command_list[1]][msg.group.id]}未在任何群发布介绍", command_list[1], msg.group.id)
             elif len(command_list) == 4:
                 if command_list[2] == "this":
                     command_list[2] == msg.group.id
@@ -1230,3 +1230,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+    w = open("introduce_data.json", 'w')
+    w.write("""{
+    "qq":{
+    
+    }
+}""")
+    w.close()
