aa7f1150a3dadf0c38ab05de6c22ff9576a91fc2
Ossicles <q2775367578@gmail.com>
2022-04-09 00:32:52 +0800

    Updated

--- main.py
+++ main.py
@@ -194,7 +194,9 @@ def read_config():
         pass
     try:
         with open('json.txt', mode="r", encoding="UTF-8") as jsonfile:
-            INTRODUCE["qq"] = json.loads(jsonfile.read())
+            json_list = json.loads(jsonfile.read())
+            INTRODUCE["qq"] = json_list["INTRODUCE"]
+            ACCOMPLISHMENT = json_list["ACCOMPLISHMENT"]
     except:
         pass
     config = configparser.ConfigParser()
