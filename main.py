44dd2d639257213e7b300a22eb4ca1f50ae4a7ce
汐洛呐 <xiluo@guimc.ltd>
2022-04-16 12:23:40 +0800

    1

--- main.py
+++ main.py
@@ -1374,7 +1374,7 @@ Coins: {coin_purse}
             msg.fast_reply(f'Success!\nEnded Time:{datetime.datetime.utcfromtimestamp(int(FOLLOW_MUTE[str(command_list[1])])).strftime("%Y-%m-%d %H:%M:%S")}')
         
         if command_list[0] == "!testcounter":
-            pass
+            msg_counter_send(msg.sender.id)
 
 
     except Exception as e:
