0263a86204d8d685461a9ffec53ba2960f2cd98c
ABCD_Creeper <cnkaile@126.com>
2022-07-18 20:26:57 +0800

    kfjaos

--- main.py
+++ main.py
@@ -1365,7 +1365,7 @@ Coins: {coin_purse}
                 msg.fast_reply("请发送QQ号或'me'!!!")
             return
 
-        if command_list[0] == "!info":
+        if msg.text == "!info":
             if msg.sender.isadmin:
                 watchdoginfo()
 
