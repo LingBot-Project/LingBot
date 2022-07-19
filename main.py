9631e944fea8aac6ec9364e28d44c925c46b37dd
ABCD_Creeper <cnkaile@126.com>
2022-07-19 08:32:54 +0800

    adf

--- main.py
+++ main.py
@@ -1370,7 +1370,7 @@ Coins: {coin_purse}
                 rt = str(threading.enumerate())
                 cpu_usage = str(psutil.cpu_times_percent().user + psutil.cpu_times_percent().system)
                 memory_usage = str(psutil.virtual_memory().percent)
-                msg.fast_reply("当前机器人运行状态:\nCPU: "+cpu_usage+"%\nMemory: "+memory_usage+"%\nRunning Threads: "+len(rt))
+                msg.fast_reply("当前机器人运行状态:\nCPU: "+cpu_usage+"%\nMemory: "+memory_usage+"%\nRunning Threads: "+str(len(rt)))
             else:
                 msg.fast_reply("您还没有权限哦")
 
