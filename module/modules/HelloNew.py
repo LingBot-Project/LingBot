82a65ca9f2d44786b0a061e9ba7fd8ab615a75b8
屑落叶 <100185989+siuank@users.noreply.github.com>
2022-07-29 19:51:35 +0800

    az

--- module/modules/HelloNew.py
+++ module/modules/HelloNew.py
@@ -6,7 +6,7 @@ class HelloNew(IModule):
     @staticmethod
     def on_message(event: GroupMessageEvent):
         if event.get_message().text == "hello ling bot!":
-            event.reply(f"hello~ {event.get_message().sender}")
+            event.reply(f"hello~ {event.get_message().sender.name}")
 
     def process(self, event: Event) -> None:
         if isinstance(event, GroupMessageEvent):
--- module/modules/TestHello.py
+++ module/modules/TestHello.py
@@ -6,7 +6,7 @@ class TestHello(IModule):
     @staticmethod
     def on_msg(event: GroupMessageEvent):
         if event.get_message().text == "!hey bot":
-            event.reply(f"hey! {event.get_message().sender}")
+            event.reply(f"hey! {event.get_message().sender.name}")
 
     def process(self, event: Event) -> None:
         if isinstance(event, GroupMessageEvent):
