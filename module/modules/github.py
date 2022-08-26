11704e1b236ee2042817faada7b2ed09761272d3
屑落叶 <100185989+siuank@users.noreply.github.com>
2022-08-26 15:51:37 +0800

    wtf

--- module/modules/github.py
+++ module/modules/github.py
@@ -73,7 +73,7 @@ sha: {commit_info[0]["sha"]}
                 Message.sendMessage(_tmp_msg, target_group=308089090, bypass=True, bypass_length=60)
             listener_last_info = str(cur_update)
         except Exception as e:
-            Message.sendMessage(f"Found an exception when try to auto-sync github commit: {traceback.format_exc()}", target_group=1019068934)
+            Message.sendMessage(f"Found an exception when try to auto-sync github commit: {traceback.format_exc()}", target_group=1019068934, bypass=True)
 
 
 class GitHubController(IModule):
