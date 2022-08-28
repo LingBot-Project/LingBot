8277c319bf97a68393b3d3cc0997fef06e1f7a6f
siuank <y2915324611@163.com>
2022-08-28 22:04:01 +0800

    e

--- module/modules/feedback.py
+++ module/modules/feedback.py
@@ -10,6 +10,8 @@ class Feedback(IModule):
     def process(self, event: Event) -> None:
         if isinstance(event, GroupMessageEvent):
             global feed_back_cooldown
+            if event.get_commands()[0] != "!feedback":
+                return
             sender = feed_back_cooldown.get(event.get_message().sender.id)
             cd = feed_back_cooldown.get(sender)
             cur_time = time.time()
@@ -24,7 +26,8 @@ class Feedback(IModule):
                 return
             # end if
             feed_back_cooldown[sender] = cur_time + 300
-            Message.sendMessage(f"用户 {sender} 提交了反馈: \n{event.get_message().text[10:]}", target_group=Message.DEV_GROUP)
+            Message.sendMessage(f"用户 {sender}() 提交了反馈: \n{event.get_message().text[10:]}", target_group=Message.DEV_GROUP)
+            event.reply("提交成功! ")
         # end if
     # end def
 # end class
